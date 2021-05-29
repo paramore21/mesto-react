45bc489160e2e3ce78b249d153aedb1c920ce62d
Fattakhova <ilnuro4ka01@gmail.com>
2021-05-29 18:51:00 +0500

    started react project

---
+++ mesto-react/src/components/ImagePopup.js
@@ -0,0 +1,13 @@
+function ImagePopup(card, onClose) {
+  return (
+    <section className={`${card.isOpen ? 'image popup popup_type_image popup_opened' : 'image popup popup_type_image '}`}>
+    <div className="image__container image__container_type_image">
+      <button type="button" className="popup__close-form" onClick={onClose}></button>
+      <img className="image__item" src={card.link} alt={card.name}/>
+      <h3 className="image__title">{card.name}</h3>
+    </div>
+  </section> 
+  )
+}
+
+export default ImagePopup
---
+++ mesto-react/src/components/Main.js
@@ -0,0 +1,61 @@
+import {useEffect, useState} from "react";
+import {api} from "../utils/Api"
+import Card from "./Card"
+
+function Main({onEditProfile, onAddPlace, onEditAvatar, handleCardClick}) {
+  const [userName, setUserName] = useState('')
+  const [userAvatar, setUserAvatar] = useState('')
+  const [userDescription, setUserDescription] = useState('')
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    api.getInitialCards().then(res => {
+      setCards(res)
+    })
+  }, [])
+
+  useEffect(() => {
+    api.getUserInformation().then(res => {setUserName(res.name)})
+  }, [])
+
+  useEffect(() => {
+    api.getUserInformation().then(res => {setUserDescription(res.about)})
+  }, [])
+
+  useEffect(() => {
+    api.getUserInformation().then(res => {setUserAvatar(res.avatar)})
+  }, [])
+  
+return (
+  <main className="main">
+    <section className="profile">
+      <p className="profile__avatar" href="#" target="_blank" onClick={onEditAvatar} style={{backgroundImage: `url(${userAvatar})`}}></p>
+      <div className="profile__info">
+        <h1 className="profile__name">{userName}</h1>
+        <h2 className="profile__description">{userDescription}</h2>
+      </div>
+      <button type="button" className="profile__edit-button" onClick={onEditProfile}></button>
+      <button type="button" className="profile__add-card" onClick={onAddPlace}></button>
+    </section>
+    <section className="elements">
+      {cards.map((card) => (
+        <Card
+          card={card}
+          link={card.link}
+          name={card.name}
+          likes={card.likes}
+          onCardClick={handleCardClick} 
+        />
+      ))}
+    </section>
+    <section className="delete popup popup_type_delete">
+      <div className="delete__container">
+        <h3 className="delete__title">Вы уверены?</h3>
+        <button type="button" className="popup__close-form"></button>
+        <button type="button" className="delete__submit popup__submit">Да</button>
+      </div>
+    </section>
+  </main>
+)}
+
+export default Main
