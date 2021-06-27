b535ae0339f808c736c440b36cd1855886f8b0a9
Fattakhova <ilnuro4ka01@gmail.com>
2021-06-27 16:50:35 +0500

    fixed after review

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ function PopupWithForm({title, children, buttonName, isOpen, formName, onClose, 
     <section className={isOpen ? `popup popup_type_${formName} popup_opened` : `popup popup_type_${formName}`}>
       <div className="popup__container">
         <h2 className="popup__edit-profile">{title}</h2>
-        <form className="popup__form" name={formName} noValidate onSubmit={onSubmit}>
+        <form className="popup__form" name={formName} onSubmit={onSubmit}>
           <button className="popup__close-form" type="button" onClick={onClose}></button>
           {children}
           <button type="submit" className="popup__submit">{buttonName}</button>
